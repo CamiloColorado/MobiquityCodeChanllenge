6ca6169953458ad8e1fddce9bdc52698043f3430
Camilo <Camilo@DESKTOP-AT7CBFG>
2021-07-15 20:01:24 -0500

    BlogComments class updated Second and third steps added

--- src/test/java/stepDefinitions/BlogComments.java
+++ src/test/java/stepDefinitions/BlogComments.java
@@ -3,8 +3,15 @@ package stepDefinitions;
 import static io.restassured.RestAssured.given;
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.NotFoundException;
+
 import io.cucumber.java.BeforeStep;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -14,6 +21,7 @@ public class BlogComments {
 	
 	private ContentType contentType;
 	private Response response;
+	private List<String> emails = new ArrayList<>();
 	
 	@BeforeStep
 	public void setUp() {
@@ -26,6 +34,24 @@ public class BlogComments {
 		RestAssured.baseURI = ConfigurationFileReader.getProperty("URI");
 		response = given().accept(contentType).param("username", userName).when().get("users").prettyPeek();
 		assertEquals(200, response.getStatusCode());
+	}
+	
+	@And("I search the post written by the user")
+	public void searchPostByUser() {
+		List<Integer> userIdList = response.jsonPath().getList("id");
+		Integer userId = userIdList.stream().findFirst()
+				.orElseThrow(() -> new NotFoundException("id not found in the response"));
+		response = given().accept(contentType).param("userId", userId).when().get("posts").prettyPeek();
+		assertEquals(200, response.getStatusCode());
+	}
+
+	@When("I get the emails in the comment section by post")
+	public void getEmails() {
+		List<Integer> postIds = response.jsonPath().getList("id");
+		postIds.stream().forEach(postId -> {
+			emails.addAll(given().accept(contentType).param("postId", postId).when().get("comments").prettyPeek().then()
+					.statusCode(200).extract().jsonPath().getList("email"));
+		});
 	}
 	
 }
